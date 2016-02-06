d25d486204062b37182dc4ea00e2f939b53bb05e
Anton Wilhelm <timaschew@gmail.com>
2016-02-06 11:30:04 +0100

    provide autocomplete

--- src/debug-helper.js
+++ src/debug-helper.js
@@ -1,7 +1,30 @@
-var jQuery = require('jquery');
+var $ = require('jquery');
+require('jquery-ui/autocomplete');
+
+// var API_LIST = require('../vendor/netsuite-api.json');
+var API_LIST = [
+  {
+    "name": "nlapiCopyRecord",
+    "params": [
+      "type",
+      "id",
+      "initializeValues"
+    ],
+    "comment": "*\n * Return a new record using values from an existing record.\n * @governance 10 units for transactions, 2 for custom records, 4 for all other records\n *\n * @param {string} \ttype The record type name.\n * @param {int} \tid The internal ID for the record.\n * @param {Object} \tinitializeValues Contains an array of name/value pairs of defaults to be used during record initialization.\n * @return {nlobjRecord}  Returns an nlobjRecord object of a copied record.\n *\n * @since\t2007.0"
+  },
+  {
+    "name": "nlapiLoadRecord",
+    "params": [
+      "type",
+      "id",
+      "initializeValues"
+    ],
+    "comment": "*\n * Load an existing record from the system.\n * @governance 10 units for transactions, 2 for custom records, 4 for all other records\n *\n * @param {string} \ttype The record type name.\n * @param {int} \tid The internal ID for the record.\n * @param {Object} \tinitializeValues Contains an array of name/value pairs of defaults to be used during record initialization.\n * @return {nlobjRecord}  Returns an nlobjRecord object of an existing NetSuite record.\n *\n * @exception {SSS_INVALID_RECORD_TYPE}\n * @exception {SSS_TYPE_ARG_REQD}\n * @exception {SSS_INVALID_INTERNAL_ID}\n * @exception {SSS_ID_ARG_REQD}\n *\n * @since\t2007.0"
+  }
+];
 
 var fetchLines = function() {
-  var allLines = jQuery('#historywindow > div').map(function(_, line) {return jQuery(line).text()});
+  var allLines = $('#historywindow > div').map(function(_, line) {return $(line).text()});
   var inputLines = allLines.filter(function(_, item) {
     return item.substr(0, 1) === '$';
   });
@@ -11,9 +34,10 @@ var fetchLines = function() {
   return inputWithoutPrompt;
 }
 
-var evalInputElement = jQuery('#evalexpression');
+var evalInputSelector = '#evalexpression';
+var evalInputElement = $(evalInputSelector);
 if (evalInputElement.length > 0) {
-  evalInputElement.attr("autocomplete", "off");
+  // evalInputElement.attr("autocomplete", "off");
   var handler = function(e) {
     if (e.keyCode == 38) {
       var items = fetchLines();
@@ -39,5 +63,29 @@ if (evalInputElement.length > 0) {
   };
   var counter = -1;
   evalInputElement.on('keyup', handler);
-  console.log('attach handler');
+
+  // inject some elements to provide better UI for autocompletion
+
+
+  evalInputElement.parent().append('<input type="hidden" id="ns-api-id">');
+  evalInputElement.parent().append('<p id="ns-api-params"></p>');
+  zzz = evalInputElement.autocomplete({
+      minLength: 0,
+      source: API_LIST,
+      focus: function( event, ui ) {
+        evalInputElement.val( ui.item.name );
+        return false;
+      },
+      select: function( event, ui ) {
+        $( evalInputSelector ).val( ui.item.name );
+        $( "#ns-api-id" ).val( ui.item.name );
+        $( "#ns-api-params" ).val( ui.item.params );
+        return false;
+      }
+    })
+    .autocomplete().data("uiAutocomplete")._renderItem = function( ul, item ) {
+      return $( "<li>" )
+        .append( "<a>" + item.name + "<br>(" + item.params.join(', ') + ")</a>" )
+        .appendTo( ul );
+    };
 }
---
+++ tools/api-json-generator.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const esprima = require('esprima')
+const fs = require('fs')
+const path = require('path')
+
+const netsuiteApi = fs.readFileSync(path.join(__dirname, 'netsuite-api.js'))
+const outPath = path.join(__dirname, '..', 'vendor', 'netsuite-api.json')
+const ast = esprima.parse(netsuiteApi, {attachComment: true})
+
+const output = []
+
+let prevComment = null
+
+ast.body.forEach((item, index) => {
+  let comment = null
+  if (item.leadingComments != null) {
+    if (index === 0) {
+      // omit header comment which is not related to any function
+      comment = item.leadingComments[1]
+    } else {
+      comment = item.leadingComments[0]
+    }
+  } else {
+    comment = prevComment
+  }
+  if (item.type === 'FunctionDeclaration') {
+    output.push({
+      name: item.id.name,
+      params: item.params.map(p => p.name),
+      comment: comment.value.trim()
+    })
+  }
+  prevComment = comment
+})
+
+fs.writeFileSync(outPath, JSON.stringify(output, null, 2), {encoding: 'utf8'})
