269192ed332269557dec6a07bb870085b5d1eea2
Anton Wilhelm <timaschew@gmail.com>
2016-02-23 22:23:56 +0100

    load "plugins" only on the pages where they needed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-require('./debug-helper');
+var jQuery = require('jquery');
+
+if (window.location.pathname == "/app/common/scripting/scriptdebugger.nl") {
+  require('./debug-helper');
+}
+
+if (
+  window.location.pathname == "/app/crm/common/crmmessage.nl" &&
+  jQuery("#messages_form").length > 0
+) {
+    require('./email-wysiwyg-injector');
+  }
